5c7411ca1a0f6e7570149094687410428ee55202
Jayesh <jayeshthakur47@gmail.com>
2025-12-15 20:35:09 +0900

    removed redundant annotation and added new handler

--- src/main/java/com/jayesh/ocms/exceptions/ControlAdvisor.java
+++ src/main/java/com/jayesh/ocms/exceptions/ControlAdvisor.java
@@ -4,8 +4,8 @@ package com.jayesh.ocms.exceptions;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -14,22 +14,33 @@ import java.util.Map;
 @RestControllerAdvice
 public class ControlAdvisor {
 
+    private static final String ERROR = "error";
+    private static final String ERROR_MESSAGE = "errorMessage";
+
     @ExceptionHandler(exception = ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String, Object>> constraintViolationExceptionHandler(ConstraintViolationException e) {
         Map<String, Object> map = new HashMap<>();
-        map.put("error", true);
-        map.put("message", "Mandatory fields missing");
+        map.put(ERROR, true);
+        map.put(ERROR_MESSAGE, "Mandatory fields missing");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
     }
 
     @ExceptionHandler(exception = NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Map<String, Object>> notFoundExceptionHandler(NotFoundException e) {
         Map<String, Object> map = new HashMap<>();
-        map.put("error", true);
-        map.put("message", e.getMessage());
+        map.put(ERROR, true);
+        map.put(ERROR_MESSAGE, e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
     }
+
+
+    @ExceptionHandler(exception = BadCredentialsException.class)
+    public ResponseEntity<Map<String, Object>> badCredentialsExceptionHandler(BadCredentialsException e) {
+        Map<String, Object> map = new HashMap<>();
+        map.put(ERROR, true);
+        map.put(ERROR_MESSAGE, e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(map);
+    }
+
 
 }
