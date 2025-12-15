bd07273602f147e6d6cc8e64bbdff4000b07f0de
Jayesh <jayeshthakur47@gmail.com>
2025-12-15 20:33:13 +0900

    added find by username

--- src/main/java/com/jayesh/ocms/repositories/UserRepository.java
+++ src/main/java/com/jayesh/ocms/repositories/UserRepository.java
@@ -5,7 +5,11 @@ import com.jayesh.ocms.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUserName(String userName);
 
 }
