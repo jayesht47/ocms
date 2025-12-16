f537d5a6254d8ee1ba0af35b59112208a500b473
Jayesh <jayeshthakur47@gmail.com>
2025-12-16 19:29:03 +0900

    added delete user API

--- src/main/java/com/jayesh/ocms/controllers/UserController.java
+++ src/main/java/com/jayesh/ocms/controllers/UserController.java
@@ -4,8 +4,9 @@ package com.jayesh.ocms.controllers;
 import com.jayesh.ocms.dto.UpdateUserDTO;
 import com.jayesh.ocms.entities.User;
 import com.jayesh.ocms.exceptions.NotFoundException;
+import com.jayesh.ocms.exceptions.RegistrationValidationException;
 import com.jayesh.ocms.services.UserService;
-import com.jayesh.ocms.dto.AuthUser;
+import com.jayesh.ocms.dto.RegisterUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,10 +23,9 @@ public class UserController {
 
 
     @PostMapping
-    public User addUser(@RequestBody AuthUser user) {
+    public User addUser(@RequestBody RegisterUser user) throws RegistrationValidationException {
         return userService.createUser(user);
     }
-
 
     @PutMapping("/{userId}")
     public User updateUser(@PathVariable("userId") String userId, @RequestBody UpdateUserDTO updateUser) throws NotFoundException {
@@ -35,6 +35,11 @@ public class UserController {
     @GetMapping("/{userId}")
     public User getUserByUserId(@PathVariable("userId") String userId) throws NotFoundException {
         return userService.getUserByUserId(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUserById(@PathVariable("userId") String userId) throws NotFoundException {
+        userService.deleteUserByUserId(userId);
     }
 
 }
